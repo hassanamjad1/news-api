b228d221b21250acc6c98cb52fddd59f15aa6394
hassanamjad1 <42243986+hassanamjad1@users.noreply.github.com>
2021-10-21 06:34:35 -0400

    Adding modules

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import News from './News/News';
+
+function App () {
+  return (
+    <div className ="App" >
+      <h1>Hey</h1>
+      <News/>
+    </div>
+  );
+  
+}
+export default App;
